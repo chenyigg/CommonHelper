f073e9444b4f82d8a52c60640eac2a64ce8311b8
陈 毅 <chenyigg123@163.com>
2021-07-21 09:38:06 +0800

    添加项目文件。

---
+++ CommonHelper/FileHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonHelper
+{
+    public class FileHelper
+    {
+        public static bool CreateNew(string path, string str)
+        {
+            if (File.Exists(path)) throw new Exception($"文件:{path}已存在！");
+            File.Create(path).Close();
+            using (FileStream fs = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+            {
+                using (StreamWriter sws = new StreamWriter(fs))
+                {
+                    sws.WriteLine(str);
+                    return true;
+                }
+            }
+
+        }
+
+
+        public static async Task<string> GetDataAsync(string path)
+        {
+
+            if (!File.Exists(path)) throw new Exception($"路径:{path}不存在！");
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    return await sr.ReadToEndAsync();
+                }
+            }
+
+        }
+
+        public static string GetData(string path)
+        {
+            if (!File.Exists(path)) throw new Exception($"路径:{path}不存在！");
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+
+        }
+
+
+        public static bool AppendData(string path, string str)
+        {
+            if (!File.Exists(path)) throw new Exception($"文件:{path}不存在！");
+            using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter sws = new StreamWriter(fs))
+                {
+                    sws.WriteLine(str);
+                    return true;
+                }
+            }
+
+        }
+    }
+}
